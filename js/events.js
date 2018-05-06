942b53d773d9239104a7167169c60dd5e9cb0824
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-05-06 20:52:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,6 +20,11 @@ function pressIt() {
   });
 }
 
+function submitIt() {
+  $('form').on('submit', function(key) {
+    alert("Your form is going to be submitted now.")
+  });
+}
 $(document).ready(function(){
 
 // call functions here
